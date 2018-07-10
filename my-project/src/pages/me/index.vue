090dd94f02c582825078b1eb06a865cb591cc0c4
王宽 <648208376@qq.com>
2018-07-10 16:54:47 +0800

    mpvue-demo

---
+++ my-project/src/pages/me/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{text}}
+    <a href="/pages/index/main" class="home">去往首页</a>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      text: 'me'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
